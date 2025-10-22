a330ade034797963f2011259940bfd288677b3d9
EnlistedSPring8 <onnzki@gmail.com>
2025-10-22 13:37:53 +0300

    C: Made the player class and added to the mainloop

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -13,6 +15,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000.0  # Delta time in seconds
